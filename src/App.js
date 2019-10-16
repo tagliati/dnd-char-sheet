61ab73a074a4631cf74cce93606961a761e51ca6
tagli <tagliati@gmail.com>
2019-10-16 08:05:05 -0300

    Add icons to basic interface

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import charSheet from "./styles";
+import { MdAddCircle, MdRemoveCircle } from "react-icons/md";
 
 function App() {
   return (
     <charSheet>
-      str
-      <input type="text" />
-      int
-      <input type="text" />
-      dex
-      <input type="text" />
-      wis
-      <input type="text" />
-      cons
-      <input type="text" />
-      char
-      <input type="text" />
+      str|int|dex|wis|cons|char
+      <button>
+        <MdAddCircle size={16} />
+      </button>
+      <button>
+        <MdRemoveCircle size={16} />
+      </button>
     </charSheet>
   );
 }
