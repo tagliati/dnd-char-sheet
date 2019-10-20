461808563a2b277f8b64da2c447ae83701d02561
tagli <tagliati@gmail.com>
2019-10-20 06:20:11 -0300

    Add basic points display

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { useState, useEfect } from "react";
+import React, { useState } from "react";
 import CharSheet from "./styles";
-import { connect } from "react-redux";
 import { MdAddCircle, MdRemoveCircle } from "react-icons/md";
 import { Provider } from "react-redux";
 
 import store from "./store";
 
-function App() {
-  const [hero, setHeroData] = useState([]);
+export default function App() {
+  const [hero, setHeroData] = useState(8);
+  const [totalPoints, setTotalPoints] = useState(27);
+
+  function handleIncreaseAttribute() {
+    setHeroData(hero + 1);
+    setTotalPoints(totalPoints - 1);
+  }
+
+  function handleDecreaseAttribute() {
+    setHeroData(hero - 1);
+    setTotalPoints(totalPoints + 1);
+  }
 
   return (
     <Provider store={store}>
       <CharSheet>
-        str|int|dex|wis|cons|char
-        <button type="button" onClick={() => this.handleIncreaseAttribute(1)}>
+        totalPoints ({totalPoints})
+        <br />
+        str({hero})|int|dex|wis|cons|char
+        <button type="button" onClick={() => handleIncreaseAttribute(1)}>
           <MdAddCircle size={16} />
         </button>
-        <button type="button" onClick={() => this.handleDecreaseAttribute(1)}>
+        <button type="button" onClick={() => handleDecreaseAttribute(1)}>
           <MdRemoveCircle size={16} />
         </button>
       </CharSheet>
     </Provider>
   );
 }
-
-export default connect()(App);
